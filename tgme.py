dc83c559804db2388e788969c9d34bb9b5e9844e
vort3 <sherigulam@gmail.com>
2022-04-20 02:00:15 +0000

    Proper handling of <, > and & characters

--- tgme.py
+++ tgme.py
@@ -33,8 +33,9 @@ def main():
         else:                    message = f"<b>{sys.argv[1]}</b>\n<i>{sys.argv[2]}</i>\n\n{' '.join(sys.argv[3:])}"
     else:
         message = "".join(sys.stdin.readlines())
-        message = message.replace("<", "_")
-        message = message.replace(">", "_")
+        message = message.replace("&", "&amp;")
+        message = message.replace("<", "&lt;")
+        message = message.replace(">", "&gt;")
         if message == "":
             return
         message = "<code>" + message + "</code>"
